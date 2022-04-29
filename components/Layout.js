705776f9d456bd4bc3024aabab927a650314f477
unknown <andreagutierrez160983@gmail.com>
2022-04-29 16:50:34 +0000

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Layout.module.css';
+
+export default function Layout() {
+    return (
+        <>
+            <h1 className={styles.blue}>Layout</h1>
+            <p>Test</p>
+        </>
+    );
+}
