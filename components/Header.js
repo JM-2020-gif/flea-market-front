b2950db8e25d3ec35115c967e22a8124ed64f4b6
unknown <andreagutierrez160983@gmail.com>
2022-06-08 19:18:44 +0000

    styles Header

--- components/Header.js
+++ components/Header.js
@@ -3,14 +3,18 @@ import styles from '@/styles/Header.module.css';
 import SearchForm from '@/components/SearchForm';
 import { menuItems } from '@/config/navigation'
 import MenuItem from '@/components/MenuItem'
+import IconMenuOpen from '../public/IconMenuOpen.svg'
 
 export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
                 <Link href="/">SECOND-HAND STORE</Link>
+                <button className={styles.mobileMenuButton}>
+                    <IconMenuOpen />
+                </button>
             </div>
-            <div>
+            <div className={styles.menuContainer}>
                 <nav className={styles.navMenu}>
                     <ul className={styles.navList}>
                         {menuItems.map((item, index) => (
@@ -18,10 +22,12 @@ export default function Header() {
                         ))}
                     </ul>
                 </nav>
+                <SearchForm />
             </div>
-            <SearchForm />
-            <div className={styles.login}>
-                <a>Log in</a>
+            <div className={styles.profile}>
+                <Link href="/">
+                    <a className={styles.loginButton}>Log in</a>
+                </Link>
             </div>
         </header>
     );
--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    images: {
-        domains: ['res.cloudinary.com'],
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
     },
-};
+    images: {
+      domains: ['res.cloudinary.com']
+    }
+  }
